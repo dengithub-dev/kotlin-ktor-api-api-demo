fb2315efd4726abb12eae1227270098a068a420a
Dennis Balinhawang <den.balinhawang@gmail.com>
2024-01-03 11:20:12 +0800

    Add additional attribute in random user for the nested

--- src/main/kotlin/den/model/Client.kt
+++ src/main/kotlin/den/model/Client.kt
@@ -12,9 +12,16 @@ data class GetDataPhilippines(
 data class ClientData(
     val id: Int,
     val uid: String,
-    val password: String
+    val password: String,
+    val address: AddressData
 )
 
+@Serializable
+data class AddressData (
+    val city: String,
+    val state: String,
+    val country: String
+)
 @Serializable
 data class Sample(
     val a: String,
